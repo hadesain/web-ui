aae571df20592c28a58ca6c951467bb06f4fa015
John Messerly <jmesserly@google.com>
2013-06-14 13:59:57 -0700

    update baselines

--- test/run.sh
+++ test/run.sh
@@ -72,7 +72,7 @@ function compare_all {
 # TODO(jmesserly): bash and dart regexp might not be 100% the same. Ideally we
 # could do all the heavy lifting in Dart code, and keep this script as a thin
 # wrapper that sets `--enable-type-checks --enable-asserts`
-  for input in $DIR/data/input/*_test.html $DIR/../example/component/news/test/*_test.html $DIR/../example/todomvc/test/*_test.html; do
+  for input in $DIR/../example/component/news/test/*_test.html $DIR/../example/todomvc/test/*_test.html; do
     if [[ ($TEST_PATTERN == "") || ($input =~ $TEST_PATTERN) ]]; then
       FILENAME=`basename $input`
       DIRNAME=`dirname $input`
